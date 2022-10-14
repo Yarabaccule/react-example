e67effb892f04eea4c98888050de9288be9f6a65
yara baccule <yarabaccule@yahoo.com.br>
2022-10-14 10:02:53 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard title="Painel de Controle" />
+    </div>
+  );
+}
+
+export default App;
